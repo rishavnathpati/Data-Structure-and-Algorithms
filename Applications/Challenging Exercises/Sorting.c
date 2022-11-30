fa3cd917e2a547d4b5a960e75b6bdb96555ab0d7
Rishav Nath Pati <patirishavnath@gmail.com>
2022-11-30 14:43:47 +0530

    Folder restructure

---
+++ Applications/Challenging Exercises/Sorting.c
@@ -0,0 +1,21 @@
+// Try to create an array of N elements as input which consists of Positive, Negative numbers and also the duplicate values. Perform any sorting procedure to get output as two sorted array one with positive numbers and one with negative numbers and print the number of comparisons made for each sorted array and aslo the say count for the each the duplicate values.
+// Exapmle : input[15]= { 10,4,-3,-1,0,4,3,-15,-8,4,-1,9,3,1,7}
+// Output1[15]={-1,-1,-3,-8,-15} and print Number of Comparisons Output2[15]={0,1,3,3,4,4,4,7,9,10} and print Number of Comparisons
+// -1 has been present twice + 3 has been present twice + 4 has been present thrice
+
+// Write a program to implement the following sorting techniques:
+// 1. Bubble sort
+// 2. Selection sort
+// 3. Insertion sort
+// 4. Merge sort
+// 5. Quick sort
+// 6. Heap sort
+// 7. Radix sort
+
+// Write a program to implement the following searching techniques:
+// 1. Linear search
+// 2. Binary search
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
